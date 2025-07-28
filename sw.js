911e5ca7b4b977c348344a8a3775520d2e7d1eda
AlchemistAssistant <myke2090@gmail.com>
2025-07-28 14:33:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('alchemist-cache').then(cache => {
+    caches.open('aa-cache-v1').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/style.css',
         '/manifest.json',
+        '/style.css',
         '/sw.js',
-        '/icon-192.png' // sau 512 etc., în funcție de ce ai
+        '/icon-192.png',
+        '/icon-284.png',
+        '/icon-512.png'
       ]);
     })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
